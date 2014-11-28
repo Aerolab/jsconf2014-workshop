7471d15cfdcca3d50346b10a0b10631a0fbf098f
Roberto González <roberto@aerolab.com.ar>
2014-11-28 01:01:01 -0300

    Fixed Step 2.

--- steps/step-2/app/remote/js/remote.js
+++ steps/step-2/app/remote/js/remote.js
@@ -1,3 +1,72 @@
 /**
  * This is the core of our Remote Control
  */
+
+// First of all, connect to the server (our desktop app)
+var socket = io.connect();
+
+// Search youtube when the user stops typing. This gives us an automatic autocomplete.
+var searchTimeout = null;
+$('#searchQuery').on('keyup', function(event){
+
+  // Cancel any queued searches
+  clearTimeout(searchTimeout);
+  // If the user pressed enter, search inmediately
+  if( event.keyCode === 13 ) {
+    // Show the loading gif and hide it when the search is done
+    var q = $('#searchQuery').val();
+    if($("#results").is(":visible")) {
+      $('.loading').show();
+      searchYoutube(q, function(){ $('.loading').hide(); });
+    }else {
+      searchHistory(q);
+    }
+  }
+  else {
+    // If not, wait a bit before searching automatically
+    searchTimeout = setTimeout(function(){
+      // Show the loading gif and hide it when the search is done
+      var q = $('#searchQuery').val();
+      if($("#results").is(":visible")){
+        $('.loading').show();
+        searchYoutube(q, function(){ $('.loading').hide(); });
+      }else{
+        searchHistory(q);
+      }
+
+    }, 500);
+  }
+});
+
+// When we click on a video, watch it on the App
+$('#results').on('click', '.video', function(event){
+  // Send an event to notify the server we want to watch this video
+  socket.emit('watchVideo', $(this).data());
+});
+
+$('#history').on('click', '.video', function(event){
+  // Send an event to notify the server we want to watch this video
+  socket.emit('watchVideo', $(this).data());
+});
+
+// When the server tells us that the player changed status (play/pause), alter the controls
+socket.on('statusChange', function(status){
+  if( status === 'play' ) {
+    $('.playback .pause').show();
+    $('.playback .play').hide();
+  }
+  else if( status === 'pause' || status === 'stop' ) {
+    $('.playback .pause').hide();
+    $('.playback .play').show();
+  }
+});
+
+// Notify the app when we hit the play button
+$('.playback .play').on('click', function(event){
+  socket.emit('play');
+});
+
+// Notify the app when we hit the pause button
+$('.playback .pause').on('click', function(event){
+  socket.emit('pause');
+});
